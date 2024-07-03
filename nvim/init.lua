63cb9dd828f09115ea9d6843c42383c524586443
krem <0xkrem@proton.me>
2024-07-03 02:58:10 +0200

    nvim:went over lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,11 +25,10 @@ vim.opt.wildmenu = true --command completion
 vim.opt.showcmd = true --display keystrokes
 vim.opt.wrap = true --wrap long lines
 vim.opt.linebreak = true --wrap lines between words
-vim.opt.signcolumn = "yes" --Sign column (left) Decrease update time vim.opt.updatetime = 250 Decrease mapped sequence wait time
+vim.opt.signcolumn = "yes" --Sign column (left)
 
 vim.opt.breakindent = true --Wrapped lines stay indented
 vim.opt.undofile = true --Persistant undo
-vim.opt.timeoutlen = 800 --timeout for sequences
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -50,6 +49,10 @@ vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.autoindent = true
 
+--keystroke sequence timeout
+vim.opt.timeoutlen = 800 --timeout for sequences
+--lsp word highlight
+vim.opt.updatetime = 250
 --[[ 
 #               #
 #    KEYMAPS    #
@@ -191,7 +194,7 @@ require("lazy").setup({
 				})
 			end, { desc = "[G]rep in [O]pen Files" })
 
-			vim.keymap.set("n", "<leader>fz", function()
+			vim.keymap.set("n", "<leader>ff", function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 					winblend = 10,
@@ -248,19 +251,23 @@ require("lazy").setup({
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
-					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
-					-- to define small helper and utility functions so you don't have to repeat yourself.
-					--
-					-- In this case, we create a function that lets us more easily define mappings specific
-					-- for LSP related items. It sets the mode, buffer and description for us each time.
+					-- This function sets the mode, buffer and description for us each time.
 					local map = function(keys, func, desc)
 						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 					end
 
+					--[[ 
+#                  #
+#    LSP REMAPS    #
+#                  #
+]]
 					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+
+					-- NOTE: This is not Goto Definition, this is Goto Declaration.
+					--  For example, in C this would take you to the header.
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Find references for the word under your cursor.
 					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
@@ -272,19 +279,19 @@ require("lazy").setup({
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("gt", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					-- map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map(
+					--[[ map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
 						"[W]orkspace [S]ymbols"
-					)
+					) ]]
 
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
@@ -292,15 +299,11 @@ require("lazy").setup({
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
 					-- or a suggestion from your LSP for this to activate.
-					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+					map("<leader>da", vim.lsp.buf.code_action, "[D]iagnostic [A]ction")
 
 					-- Opens a popup that displays documentation about the word under your cursor
 					--  See `:help K` for why this keymap.
-					map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
-					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					map("<leader>rt", vim.lsp.buf.hover, "Hover Documentation")
 
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
@@ -360,18 +363,21 @@ require("lazy").setup({
 			--  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+			--[[ 
+#                       #
+#    MASON INSTALLER    #
+#                       #
+]]
 			local servers = {
+				bashls = {},
+				html = {},
+				tsserver = {},
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`tsserver`) will work just fine
 				-- tsserver = {},
+				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 
 				lua_ls = {
