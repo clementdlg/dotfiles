542e18d8b13bec3ed7a60abbf173fe1624171a59
krem <0xkrem@proton.me>
2024-08-10 20:36:43 +0200

    update on awesome's rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -86,7 +86,7 @@ tag_icons[7] = ""
 modkey = "Mod1"
 
 -- Windows gaps
-beautiful.useless_gap = 6
+beautiful.useless_gap = 8
 
 --[[---------------
 |			      |
@@ -101,31 +101,9 @@ awful.layout.layouts = {
 	awful.layout.suit.floating,
 }
 
--- {{{ Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
-	{
-		"hotkeys",
-		function()
-			hotkeys_popup.show_help(nil, awful.screen.focused())
-		end,
-	},
-	{ "manual", terminal .. " -e man awesome" },
-	{ "edit config", editor_cmd .. " " .. awesome.conffile },
-	{ "restart", awesome.restart },
-	{
-		"quit",
-		function()
-			awesome.quit()
-		end,
-	},
-}
-
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
 -- {{{ Wibar
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
@@ -370,15 +348,6 @@ awful.screen.connect_for_each_screen(function(s)
 		},
 	})
 end)
--- }}}
-
--- {{{ Mouse bindings
---root.buttons(gears.table.join(
---	--awful.button({}, 3, function()
---	--end),
---	awful.button({}, 4, awful.tag.viewnext),
---	awful.button({}, 5, awful.tag.viewprev)
---))
 -- }}}
 
 -- {{{ Key bindings
@@ -704,25 +673,6 @@ globalkeys = gears.table.join(
 		{ description = "Print screen", group = "Multimedia" }
 	),
 
-	-- -- Prompt
-	-- awful.key({ modkey }, "r", function()
-	-- 	awful.screen.focused().mypromptbox:run()
-	-- end, { description = "run prompt", group = "launcher" }),
-
-	--}),
-	--awful.key({ modkey }, "x", function()
-	--	awful.prompt.run({
-	--		prompt = "Run Lua code: ",
-	--		textbox = awful.screen.focused().mypromptbox.widget,
-	--		exe_callback = awful.util.eval,
-	--		history_path = awful.util.get_cache_dir() .. "/history_eval",
-	--	})
-	--end, { description = "lua execute prompt", group = "awesome" }),
-	-- Menubar
-	--awful.key({ modkey }, "p", function()
-	--	menubar.show()
-	--end, { description = "show the menubar", group = "launcher" })
-
 	--[[-----------------------
 	|						  |
 	|	MULTIMEDIA BINDINGS   |
@@ -851,34 +801,31 @@ clientkeys = gears.table.join(
 	awful.key({ modkey, "Control" }, "Return", function(c)
 		c:swap(awful.client.getmaster())
 	end, { description = "move to master", group = "client" }),
+
 	--awful.key({ modkey }, "o", function(c)
 	--c:move_to_screen()
 	--end, { description = "move to screen", group = "client" }),
+
 	--awful.key({ modkey }, "t", function(c)
 	--c.ontop = not c.ontop
 	--end, { description = "toggle keep on top", group = "client" }),
+
 	awful.key({ modkey }, "n", function(c)
-		-- The client currently has the input focus, so it cannot be
-		-- minimized, since minimized clients can't have the focus.
 		c.minimized = true
 	end, { description = "minimize", group = "client" }),
+
 	awful.key({ modkey }, "m", function(c)
 		c.maximized = not c.maximized
 		c:raise()
 	end, { description = "(un)maximize", group = "client" })
-	--awful.key({ modkey, "Control" }, "m", function(c)
-	--	c.maximized_vertical = not c.maximized_vertical
-	--	c:raise()
-	--end, { description = "(un)maximize vertically", group = "client" }),
-	--awful.key({ modkey, "Shift" }, "m", function(c)
-	--	c.maximized_horizontal = not c.maximized_horizontal
-	--	c:raise()
-	--end, { description = "(un)maximize horizontally", group = "client" })
 )
 
--- Bind all key numbers to tags.
--- Be careful: we use keycodes to make it work on any keyboard layout.
--- This should map on the top row of your keyboard, usually 1 to 9.
+--[[--------------
+|			     |
+|	WORKSPACES   |
+|			     |
+----------------]]
+
 local max_workspace = 9
 local tag_keys = { "&", "é", '"', "'", "(", "-", "è", "_", "ç" }
 for i = 1, max_workspace do
