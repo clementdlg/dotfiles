7fce28f4e313af29eb9d807cf5ed584a2ae7d788
krem <0xkrem@proton.me>
2024-08-14 18:09:29 +0200

    changed binding

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -454,7 +454,7 @@ clientkeys = gears.table.join(
 		c.fullscreen = not c.fullscreen
 		c:raise()
 	end, { description = "toggle fullscreen", group = "client" }),
-	awful.key({ modkey, "Shift" }, "c", function(c)
+	awful.key({ modkey, "Shift" }, "q", function(c)
 		c:kill()
 	end, { description = "close", group = "client" }),
 	awful.key(
