a9f5850a5d8491ddd86cd8b706a3a596042b7e5e
Clément DE LA GENIERE <0xkrem@proton.me>
2025-01-19 00:53:33 +0100

    refactored bash config with error handling

--- bash/bash_tmux.sh
+++ bash/bash_tmux.sh
@@ -1,47 +1,32 @@
 #!/bin/env bash
-# centrazed all the tmux conf
+# tmux environment conf
 
 function tmuxMain() {
 	# exit if tmux is not installed
-	which tmux &>/dev/null
-	if [[ $? -ne 0 ]]; then
-		return
-	fi
+	silent which tmux || return
 
 	# check if inside of tmux
 	if [[ -n $TMUX ]]; then
-		# inside of tmux
-		setTmuxPrompt
+		PS1='\[\e[38;5;153;1m\] \[\e[0m\]' # inside of tmux
 	else
-		# not inside of tmux
-		openSession
+		openSession # not inside of tmux
 	fi
-
-	unset -f tmuxMain # forget the function
 }
 
 function openSession() {
-	silent tmux list-sessions
-	local session="$?"
-	local clients=$(tmux list-clients)
-
-	if [[ $session -eq 0 ]]; then # if their is a session
-		if [[ -z $clients ]]; then # but no clients attached
+	if silent tmux list-sessions; then # if their is a session
+		if [[ -z "$(tmux list-clients)" ]]; then # but no clients attached
 			silent tmux attach
 		fi
-	else
-		silent tmux new-session # this is no session
+		return
 	fi
-	unset -f openSession # forget the function
+	silent tmux new-session # this is no session
 }
 
-function setTmuxPrompt() {
-	PS1='\[\e[38;5;153;1m\] \[\e[0m\]'
-	unset -f setTmuxPrompt # forget the function
-}
-
-function silent() {
-	"$@" &>/dev/null
-}
+silent() { "$@" &>/dev/null ;}
 
 tmuxMain
+# forget the functions
+unset -f tmuxMain
+unset -f openSession
+unset -f silent
