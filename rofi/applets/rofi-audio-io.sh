ce2b02dcb140553eede625e9f6bced75e7bf5e5b
Clément DE LA GENIERE <0xkrem@proton.me>
2025-03-11 11:02:14 +0100

    hyprland: fix workspaces

---
+++ rofi/applets/rofi-audio-io.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+set -xueo pipefail
+
+theme="$HOME/.config/rofi/theme/main.rasi"
+
+get_title() {
+	# currently playing
+	local play_status="$(playerctl status)"
+	local title=""
+	if [[ "$play_status" != "Stopped" ]]; then
+		title="$(playerctl metadata --format "{{ title }} - {{ artist }}")"
+	fi
+}
+
+rofi_cmd() {
+	title="$(get_title)"
+
+	if [[ -z "$title" ]]; then
+		rofi show -dmenu -i -p "Audio I/O" -theme "${theme}"
+	else
+		rofi show -dmenu -i -p "Audio I/O" -theme " ${theme}" -mesg "$title"
+	fi
+}
+
+is_installed() {
+	[[ -z "$1" ]] && return 1
+
+	if ! which "$1" &>/dev/null; then
+		echo "error : $1 is not installed"
+		return 1
+	fi
+	return 0
+}
+
+notify() {
+	local message="$1"
+	[[ -z "$message" ]] && return
+
+	icon="/usr/share/icons/Papirus/48x48/status/notification-audio-volume-medium.svg"
+	notify-send -i "$icon" "Bluetooth Applet" "$message"
+}
+
+get_sink_description() {
+    local sink_name="$1"
+	local sink="$(pactl list sinks | grep -A1 "Name: $sink_name" | sed 1d | cut -f2 -d:)"
+	echo "${sink:1}"
+}
+
+get_source_description() {
+    local source_name="$1"
+	local source="$(pactl list sources | grep -A1 "Name: $source_name" | sed 1d | cut -f2 -d:)"
+	echo "${source:1}"
+}
+
+
+get_sink_by_description() {
+    local description="$1"
+	local sink="$(pactl list sinks | grep -B1 "Description: $description" | sed 2d | cut -f2 -d:)"
+	echo "${sink:1}"
+}
+
+get_source_by_description() {
+    local description="$1"
+	local source="$(pactl list sources | grep -B1 "Description: $description" | sed 2d | cut -f2 -d:)"
+	echo "${source:1}"
+}
+
+
+get_outputs() {
+    pactl list sinks short | while read -r line; do
+        local sink_name=$(echo "$line" | awk '{print $2}')
+        local description="$(get_sink_description "$sink_name")"
+        echo "$description"
+    done
+}
+
+get_inputs() {
+    pactl list sources short | grep -v monitor | while read -r line; do
+        local source_name=$(echo "$line" | awk '{print $2}')
+        local description="$(get_source_description "$source_name")"
+        echo "$description"
+    done
+}
+
+
+input_menu() {
+    local inputs=$(get_inputs)
+    
+    if [ -z "$inputs" ]; then
+        notify "no audio input devices found."
+        exit 0
+    fi
+
+	local choice="$(echo "$inputs" | rofi_cmd "audio inputs" )"
+
+	[[ -z "$choice" ]] && return 0
+
+	local source_name="$(get_source_by_description "$choice")"
+
+    if [ -n "$source_name" ]; then
+        pactl set-default-source "$source_name" 
+        notify "input : $choice"
+    fi
+}
+
+output_menu() {
+    local outputs=$(get_outputs)
+    
+    if [ -z "$outputs" ]; then
+        notify "no audio output devices found."
+        exit 0
+    fi
+
+	local choice="$(echo "$outputs" | rofi_cmd "audio outputs" )"
+
+	[[ -z "$choice" ]] && return 0
+
+	local sink_name="$(get_sink_by_description "$choice")"
+
+    if [ -n "$sink_name" ]; then
+        pactl set-default-sink "$sink_name" 
+        notify "output : $choice"
+    fi
+}
+
+
+is_muted() {
+	local device="$1"
+
+	cmd="$(amixer get "$device" | grep -oE '\[on\]|\[off\]' | sed 1d)"
+
+	if [[ "$cmd" == "[off]" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+main() {
+	is_installed "pactl"
+	is_installed "notify-send"
+	is_installed "rofi"
+	is_installed "playerctl"
+	
+	# menu items
+	local outputs="󰓃 Audio Outputs 󰜴"
+	local inputs=" Audio Inputs 󰜴"
+
+	local speakers=" Mute Speakers"
+	local mic=" Mute Microphone"
+
+	is_muted "Master" && speakers=" Unmute Speakers"
+	is_muted "Capture" && mic=" Unmute Microphone"
+
+	local gui_app=" Audio Mixer"
+	local menu="$outputs\n$inputs\n$speakers\n$mic\n"
+
+	# display rofi menu
+	local choice="$(printf "$menu" | rofi_cmd)"
+
+	case "$choice" in
+		"$outputs")
+			output_menu ;;
+		"$inputs")
+			input_menu ;;
+		"$speakers")
+			amixer set Master toggle ;;
+		"$mic")
+			amixer set Capture toggle ;;
+	esac
+}
+main
---
+++ rofi/applets/rofi-powermenu.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+rofi_cmd() {
+	rofi show -dmenu -i -p "Power Menu" -theme "${theme}" -mesg "$uptime"
+}
+
+main() {
+	theme="$HOME/.config/rofi/theme/main.rasi"
+	local lockscreen=" Lock Screen"
+	local logoff=" Log Out"
+	local shutdown=" Power Off"
+	local restart=" Restart"
+	local uptime="$(uptime -p | sed -e 's/up /Uptime: /g')"
+
+	local menu="$lockscreen\n$logoff\n$shutdown\n$restart\n"
+	choice="$(printf "$menu" | rofi_cmd )"
+
+	case "$choice" in
+		"$lockscreen")
+			xfce4-screensaver-command -l ;;
+		"$logoff")
+			xfce4-session-logout --logout ;;
+		"$shutdown")
+			systemctl poweroff ;;
+		"$restart")
+			systemctl reboot ;;
+	esac
+}
+
+main
