1b153f765447c4783eb3383d5401ba2a0e89ee2e
Clément DE LA GENIERE <0xkrem@proton.me>
2025-11-15 14:23:07 +0100

    working mirror mode monitors.sh

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+# set -x
+
+##################"
+
+# mirror #1 on #2
+# reset everything
+
 # monitor = name, resolution, position, scale
 
 set_mirror() {
+	echo "Mirroring eDP-1 -> $_EXTERNAL_NAME"
 	hyprctl keyword monitor ${builtin_conf}, mirror, $_EXTERNAL_NAME
 
+}
+
+reset_builtin() {
+	echo "Resetting eDP-1"
+	hyprctl keyword monitor $builtin_conf
 }
 
 get_external_monitor_name() {
@@ -17,19 +30,29 @@ get_external_monitor_name() {
 	_EXTERNAL_NAME="$external_name"
 }
 
+search_monitors() {
+	set -u
+	target_name="$1"
+	set +u
+
+	echo "${_MONITOR_TABLE[@]}" | grep -qw "$target_name"
+}
+
 main() {
-	buildin_name="Chimei Innolux Corporation 0x1529"
-	builtin_conf="desc:${buildin_name}, highres@highrr, auto-down, 1"
+	builtin_name="eDP-1"
+	builtin_conf="${builtin_name}, highres@highrr, auto-down, 1"
 
 	_MONITOR_TABLE=($(hyprctl monitors -j | jq -r '.[].name'))
 	monitors_count="${#_MONITOR_TABLE[@]}"
 
-	if (( monitors_count == 2 )); then
-		echo "2 monitors detected"
+	# declare -p _MONITOR_TABLE # debug
+
+	if (( monitors_count == 2 )) && search_monitors "$builtin_name"; then
 		get_external_monitor_name 
 		set_mirror 
 	else 
-		echo "count = $monitors_count"
+		# echo "count = $monitors_count" # debug
+		reset_builtin 
 	fi
 }
 
